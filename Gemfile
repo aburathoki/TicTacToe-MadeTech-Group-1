56380a2ec9822bab13d6e9b31ebc59409284beba
asha burathoki <asha.burathoki@madetech.com>
2022-03-17 14:48:01 +0000

    test file and one test added

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem "sinatra", "~> 2.2"
 gem "rspec", "~> 3.11"
 
 gem 'rspec_junit_formatter'
+gem "capybara", "~> 3.36"
---
+++ spec/server_spec.rb
@@ -0,0 +1,8 @@
+Capybara.app = Sinatra::Application
+
+describe 'home page', type: :feature do
+  it 'displays correct text' do
+    visit '/'
+    expect(page).to have_content("deployment with ci")
+  end
+end
